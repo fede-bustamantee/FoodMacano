40138fd29327f3104b5c185bfe2d7b2044d4eef8
fede bustamante <federicobustamantevyp@gmail.com>
2024-10-17 10:37:48 -0300

    Icon

--- BackFoodMacano/Controllers/CarritoComprasController.cs
+++ BackFoodMacano/Controllers/CarritoComprasController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BackFoodMacano.DataContext;
 using FoodMacanoServices.Models;
@@ -25,7 +20,9 @@ namespace BackFoodMacano.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<CarritoCompra>>> GetcarritoCompra()
         {
-            return await _context.carritoCompra.ToListAsync();
+            return await _context.carritoCompra
+                .Include(c => c.Producto)
+                .ToListAsync();
         }
 
         // GET: api/CarritoCompras/5
